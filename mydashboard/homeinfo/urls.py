b9cd3f885dd4a0893ed5deb80890426dd276e513
kiruba.r@gmail.com <Learn@2020>
2020-04-17 16:38:58 -0400

    Started adding code to request external api.

--- mydashboard/homeinfo/urls.py
+++ mydashboard/homeinfo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomeInfoList, HomeInfoDetails
+from .views import HomeInfoList, HomeInfoDetails, homeinfo
 urlpatterns = [
     path('homeinfo/', HomeInfoList.as_view()),
     path('homedetails/<int:pk>/', HomeInfoDetails.as_view()),
+    path('home/', homeinfo, name='home'),
 ]
--- mydashboard/homeinfo/views.py
+++ mydashboard/homeinfo/views.py
@@ -1,6 +1,7 @@
 from homeinfo.models import HomeInfo
 from homeinfo.serializers import HomeInfoSerializer, HomeSerializer
 from django.shortcuts import render
+from .models import HomeInfo
 
 from rest_framework import generics
 
@@ -14,3 +15,9 @@ class HomeInfoDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = HomeInfo.objects.all()
     serializer_class = HomeSerializer
 
+
+def homeinfo(request):
+    if request.method == 'POST':
+        home = HomeInfo.objects.all()
+        return render(request, 'homeinfo.html', {'data': home, 'sent': True})
+    return render(request, 'homeinfo.html', content_type=None)
