61de642cac65b0c8a9ccdd99b89a91e31081eecb
kirupa <kiruba.r@gmail.com>
2020-08-27 21:57:25 -0400

    Adding Utility App.

--- mydashboard/base/logging_setup.py
+++ mydashboard/base/logging_setup.py
@@ -3,62 +3,62 @@ import sys
 
 
 class BaseFilter(logging.Filter):
-    app_name=None
-    space=None
+    app_name = None
+    space = None
 
     def filter(self, record):
-        record.app_name=self.app_name
-        record.space=self.space
+        record.app_name = self.app_name
+        record.space = self.space
         return True
 
 
 def generate_logging_config():
 
     class Filter(BaseFilter):
-        app_name = 'mydashboard'
+        app_name = 'homeutils'
         space = 'DEV'
 
     logging_config = \
         {
-        'version': 1,
-        'disable_existing_loggers': False,
-        'formatters': {
-            'verbose': {
-                'class': 'logging.Formatter',
-                'format': '{levelname} timestamp=%{asctime}s {module} {process:d} {thread:d} {message}',
-                'style': '{',
+            'version': 1,
+            'disable_existing_loggers': False,
+            'formatters': {
+                'default': {
+                    'class': 'logging.Formatter',
+                    'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+                    'style': '{'
+                },
+                'default1': {
+                    'class': 'logging.Formatter',
+                    'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+                    'style': '{'
+                },
             },
-            'default': {
-            'format': '{levelname} {message}',
-            'style': '{',
+            'filters': {'base_filter': {'()': Filter},
+                        },
+            'handlers': {
+                'stdout': {
+                    'class': 'logging.StreamHandler',
+                    'formatter': 'default',
+                    'stream': sys.stdout,
+                    'filters': ['base_filter'],
+                },
             },
-        },
-        'filters': {
-            'base_filter': {'()': Filter},
-        },
-        'handlers': {
-            'stdout': {
-                'class': 'logging.StreamHandler',
-                'formatter': 'verbose',
-                'stream': sys.stdout,
-                'filters': ['base_filter'],
+            'root': {
+                'handlers': ['stdout'],
+                'level': 'DEBUG',
             },
-        },
-        'root': {
-            'handlers': ['stdout'],
-            'level': 'DEBUG',
-        },
-        'loggers': {
-            'django': {
-            'handlers': ['stdout'],
-            'level': 'INFO',
-            'propagate': False,
-        },
-            'django.request': {
-            'handlers': ['stdout'],
-            'level': 'ERROR',
-            'propagate': False,
-        },
+            'loggers': {
+                'django': {
+                    'handlers': ['stdout'],
+                    'level': 'INFO',
+                    'propagate': False,
+                },
+                'django.request': {
+                    'handlers': ['stdout'],
+                    'level': 'ERROR',
+                    'propagate': False,
+                },
+            },
         }
-    }
     return logging_config
--- mydashboard/base/settings/common.py
+++ mydashboard/base/settings/common.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-from logging import config
+from logging.config import dictConfig
 from base.logging_setup import generate_logging_config
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -27,13 +27,10 @@ SECRET_KEY = '82&rk+n&n!um_2=gly57dzcxxx@-hh_5flh&w50fn*yns=@y_k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-
 LOGING_CONFIG = None
 
-config.dictConfig(generate_logging_config())
-print(generate_logging_config())
 
+dictConfig(generate_logging_config())
 
 ALLOWED_HOSTS = []
 
